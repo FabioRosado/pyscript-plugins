eb7a009bbeacb5ba264339ccdb2560818cd3c888
FabioRosado <fabiorosado@outlook.com>
2022-12-20 11:44:09 +0000

    update plugin

--- python/hello-world.py
+++ python/hello-world.py
@@ -6,11 +6,7 @@ plugin = Plugin("PyHelloWorld")
 class PyHelloWorld:
     def __init__(self, element):
         self.element = element
-    
-    def _create_element(self):
-        el = js.document.createElement("div")
-        el.innerHTML = "Hello World!"
-        return el
+
     
     def connect(self):
-        self._create_element()
+        self.element.innerHTML = "<div id='hello'>Hello World!</div>"
