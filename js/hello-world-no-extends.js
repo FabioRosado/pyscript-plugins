2402ea9cb1f05525d9e441c25cf8cb5464f147f4
FabioRosado <fabiorosado@outlook.com>
2022-12-20 23:11:43 +0000

    add js without extends

---
+++ js/hello-world-no-extends.js
@@ -0,0 +1,44 @@
+export default class HelloWorldPlugin {
+
+    configure(config) {
+
+    }
+    
+
+    beforeLaunch(config) {
+        customElements.define('py-hello-world', PyHelloWorld);
+        this.elem = document.createElement('py-hello-world');
+        document.body.append(this.elem);
+    }
+
+    afterStartup(runtime) {
+        // if (this.autoclose) {
+        //     this.elem.close();
+        // }
+        customElements.define('py-hello-world', PyHelloWorld);
+        this.elem = document.createElement('py-hello-world');
+        document.body.append(this.elem);
+    }
+
+    onUserError(error) {
+        // if (this.elem !== undefined) {
+        //     // Remove the splashscreen so users can see the banner better
+        //     this.elem.remove();
+        // }
+    }
+}
+
+class PyHelloWorld extends HTMLElement {
+    id = 'test';
+    mount_name;
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `<h1>Hello, world!</h1>`;
+        this.mount_name = this.id
+    }
+
+}
+
